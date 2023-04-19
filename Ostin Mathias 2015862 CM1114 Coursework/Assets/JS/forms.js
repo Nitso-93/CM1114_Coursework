181d0e4cceefdaeb2226718eaea0a805cc33dc64
Ostin <91962782+Nitso-93@users.noreply.github.com>
2023-04-19 15:51:45 +0100

    Add files via upload

---
+++ Ostin Mathias 2015862 CM1114 Coursework/Assets/JS/forms.js
@@ -0,0 +1,18 @@
+function newsletter()
+{
+  alert('Thank you for subscribing to our newsletter');
+  return false;
+}
+
+function membership()
+{
+  alert('Thank you for your membership application. We will get in touch with you soon');
+  return false;
+}
+
+function contact()
+{
+  alert('We have recieved your inquiry and will get in touch with you shortly');
+  return false;
+}
+//plot twist the forms never go through ;d
